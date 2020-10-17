94f4ec4483734fbbccc8c067b9350fa666766f63
Christopher <krizivan03@yahoo.com>
2020-10-17 15:23:21 -0700

    i dont know that my services work. StockQuote was honestly the same as yours CrimeData uses threads.

--- CrimeDataService/Service1.svc.cs
+++ CrimeDataService/Service1.svc.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Net.Http;
+
+using Newtonsoft.Json.Linq;
+using System.Globalization;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.Serialization;
@@ -12,22 +16,31 @@ namespace CrimeDataService
     // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
     public class Service1 : IService1
     {
-        public string GetData(int value)
+        public string responseData;
+        public string getCrimeData(string lon, string lat)
         {
-            return string.Format("You entered: {0}", value);
+            getData(lon, lat);
+            
+            return responseData;
         }
 
-        public CompositeType GetDataUsingDataContract(CompositeType composite)
+            public async void getData(string lon, string lat)
         {
-            if (composite == null)
+            
+             var baseAddress = new Uri("https://api.crimeometer.com/");
+
+            using (var httpClient = new HttpClient { BaseAddress = baseAddress })
             {
-                throw new ArgumentNullException("composite");
+
+                httpClient.DefaultRequestHeaders.TryAddWithoutValidation("x-api-key", "Nn3Pm4bq7kt4F2H9sgvT3i2eZCH10RI97Pz6mnr2");
+
+                using (var response = await httpClient.GetAsync($"v1/incidents/raw-data?lat={lat}&lon={lon}&distance=10mi&datetime_ini=2020-01-01&datetime_end=&page="))
+                {
+
+                    this.responseData = await response.Content.ReadAsStringAsync();
+                }
             }
-            if (composite.BoolValue)
-            {
-                composite.StringValue += "Suffix";
-            }
-            return composite;
         }
+
     }
 }
--- StockQuoteService/Service1.svc.cs
+++ StockQuoteService/Service1.svc.cs
@@ -5,6 +5,7 @@ using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.ServiceModel.Web;
 using System.Text;
+using Newtonsoft.Json.Linq;
 
 namespace StockQuoteService
 {
@@ -12,22 +13,30 @@ namespace StockQuoteService
     // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
     public class Service1 : IService1
     {
-        public string GetData(int value)
+        public string[] getStockData(string ticker)
         {
-            return string.Format("You entered: {0}", value);
+            var baseAddress = new Uri("https://cloud.iexapis.com");
+            var Token = "?token=pk_e96bc5b439e149dc9f58c081bffffc88";
+
+            string url = String.Format(
+                "https://cloud.iexapis.com/stable/stock/{0}/quote?token={1}",
+                ticker,
+                Token);
+
+            List<string> details = new List<string>();
+            JObject jsonData;
+            using (var client = new System.Net.WebClient())
+            {
+                jsonData = JObject.Parse(client.DownloadString(url));
+            }
+            if (jsonData.SelectToken("cod").ToString() == "200")
+            {
+                Console.WriteLine(jsonData.ToString());
+
+            }
+
+            return details.ToArray();
         }
 
-        public CompositeType GetDataUsingDataContract(CompositeType composite)
-        {
-            if (composite == null)
-            {
-                throw new ArgumentNullException("composite");
-            }
-            if (composite.BoolValue)
-            {
-                composite.StringValue += "Suffix";
-            }
-            return composite;
-        }
     }
 }
