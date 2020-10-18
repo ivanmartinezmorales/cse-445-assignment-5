37c740b9af26719e42f816f8262e1ed012e288ef
Ivan Martinez-Morales <ivan.martinez.morales@gmail.com>
2020-10-17 20:41:34 -0700

    Done with weather service

--- WeatherService/Service1.svc.cs
+++ WeatherService/Service1.svc.cs
@@ -5,7 +5,7 @@ using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.ServiceModel.Web;
 using System.Text;
-using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
 
 namespace WeatherService
 {
@@ -14,38 +14,110 @@ namespace WeatherService
         private string apiKey = "9c358ce4606b4993c8ae45efda970328";
         public string[] Weather5day(int zipCode)
         {
-            string url = String.Format(
-                "http://api.openweathermap.org/data/2.5/forecast?zip={0},us&appid={1}",
-                zipCode,
-                apiKey);
+            string url = string.Format("http://api.openweathermap.org/data/2.5/forecast?zip={0},us&appid={1}", zipCode, apiKey);
 
+            Root o = new Root();
             List<string> details = new List<string>();
-            JObject jsonData;
             using (var client = new System.Net.WebClient())
             {
-                jsonData = JObject.Parse(client.DownloadString(url));
-            }
-            if (jsonData.SelectToken("cod").ToString() == "200")
-            {
-                Console.WriteLine(jsonData.ToString());
+                var jsonData = client.DownloadString(url);
+                o = JsonConvert.DeserializeObject<Root>(jsonData);
+                foreach (var item in o.List)
+                {
+                    details.Add(k2f(item.Main.Temp).ToString());
+                }
 
+                foreach (var item in o.List)
+                {
+                    details.Add(item.Weather.First().Description);
+                }
             }
 
             return details.ToArray();
         }
 
-        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        private static double k2f(double temp)
         {
-            if (composite == null)
-            {
-                throw new ArgumentNullException("composite");
-            }
-            if (composite.BoolValue)
-            {
-                composite.StringValue += "Suffix";
-            }
-            return composite;
+            return (temp - 273.15) * (9 / 5) + 32;
         }
 
     }
+
+    public class Main
+    {
+        public double Temp { get; set; }
+        public double FeelsLike { get; set; }
+        public double TempMin { get; set; }
+        public double TempMax { get; set; }
+        public int Pressure { get; set; }
+        public int SeaLevel { get; set; }
+        public int GrndLevel { get; set; }
+        public int Humidity { get; set; }
+        public double TempKf { get; set; }
+    }
+
+    public class Weather
+    {
+        public int Id { get; set; }
+        public string Main { get; set; }
+        public string Description { get; set; }
+        public string Icon { get; set; }
+    }
+
+    public class Clouds
+    {
+        public int All { get; set; }
+    }
+
+    public class Wind
+    {
+        public double Speed { get; set; }
+        public int Deg { get; set; }
+    }
+
+    public class Sys
+    {
+        public string Pod { get; set; }
+    }
+
+    public class List
+    {
+        public int Dt { get; set; }
+        public Main Main { get; set; }
+        public List<Weather> Weather { get; set; }
+        public Clouds Clouds { get; set; }
+        public Wind Wind { get; set; }
+        public int Visibility { get; set; }
+        public int Pop { get; set; }
+        public Sys Sys { get; set; }
+        public string DtTxt { get; set; }
+    }
+
+    public class Coord
+    {
+        public double Lat { get; set; }
+        public double Lon { get; set; }
+    }
+
+    public class City
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Coord Coord { get; set; }
+        public string Country { get; set; }
+        public int Population { get; set; }
+        public int Timezone { get; set; }
+        public int Sunrise { get; set; }
+        public int Sunset { get; set; }
+    }
+
+    public class Root
+    {
+        public string Cod { get; set; }
+        public int Message { get; set; }
+        public int Cnt { get; set; }
+        public List<List> List { get; set; }
+        public City City { get; set; }
+    }
+
 }
